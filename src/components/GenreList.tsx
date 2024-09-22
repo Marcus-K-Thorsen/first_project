b5b4b4d6a6d4151832bcf34a0152b0de112b6fc8
Marcus <marc303i@stud.kea.dk>
2024-09-22 17:00:21 +0200

    made it so genres are displayed in the left side of the screen

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,27 @@
 import useGenres from "../hooks/useGenres"
-import { Text } from "@chakra-ui/react";
+import { Text, List, ListItem, HStack, Image } from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
     const { data: genres, error, isLoading } = useGenres();
 
     return (
-        <>
+        <List>
             {genres.map((genre) => (
-                <Text key={genre.id}>{genre.name}</Text>
+                <ListItem key={genre.id} padding="5px">
+                    <HStack>
+                        <Image
+                            boxSize="32px"
+                            borderRadius={8}
+                            objectFit="cover"
+                            src={getCroppedImageUrl(genre.image_background)}
+                        />
+                        <Text fontSize="lg">{genre.name}</Text>
+                    </HStack>
+                </ListItem>
+                
             ))}
-        </>
+        </List>
     )
 };
 
